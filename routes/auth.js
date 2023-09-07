2e18e24dfe5bd64f413002fab57ea8b17d1cf496
Priyojit12 <priyanka2591989>
2023-09-07 10:13:37 +0530

    commit

--- routes/auth.js
+++ routes/auth.js
@@ -16,7 +16,7 @@ router.post(
   ],
   async (req, res) => {
     let success = false;
-
+ 
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ success,errors: errors.array() });
